537d31c96d422a7e4197831bba2977dccb7a0c5e
haibo.wang <haibo.wang@unity.cn>
2026-03-16 17:43:09 +0800

    Remove editor type references from runtime assemblies
    - Removed BuildFromEditorGraph methods that reference editor types
    - Runtime assemblies must not reference editor types (TaskGraph, RenderGraph, etc.)
    - Build logic should be in Importer classes instead
    - Added [SerializeReference] attribute for polymorphic serialization

--- Assets/Tutorials/03_ExecutionFlow/Runtime/TaskRuntimeGraph.cs
+++ Assets/Tutorials/03_ExecutionFlow/Runtime/TaskRuntimeGraph.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using GraphToolkitTutorials.ExecutionFlow;
 
 namespace GraphToolkitTutorials.ExecutionFlow.Runtime
 {
@@ -14,38 +13,13 @@ namespace GraphToolkitTutorials.ExecutionFlow.Runtime
         /// <summary>
         /// 所有运行时节点
         /// </summary>
+        [SerializeReference]
         public List<TaskRuntimeNode> nodes = new List<TaskRuntimeNode>();
 
         /// <summary>
         /// 起始节点索引
         /// </summary>
         public int startNodeIndex = -1;
-
-        /// <summary>
-        /// 从编辑器图形构建运行时图形
-        /// </summary>
-        public void BuildFromEditorGraph(TaskGraph editorGraph)
-        {
-            nodes.Clear();
-
-            // 转换所有节点
-            foreach (var editorNode in editorGraph.Nodes)
-            {
-                if (editorNode is TaskNode taskNode)
-                {
-                    var runtimeNode = taskNode.CreateRuntimeNode(editorGraph);
-                    nodes.Add(runtimeNode);
-
-                    // 记录起始节点索引
-                    if (editorNode is StartNode)
-                    {
-                        startNodeIndex = nodes.Count - 1;
-                    }
-                }
-            }
-
-            Debug.Log($"Built runtime graph with {nodes.Count} nodes, start index: {startNodeIndex}");
-        }
 
         /// <summary>
         /// 获取指定索引的节点
--- Assets/Tutorials/09_RenderGraphBasics/Runtime/RenderGraphRuntime.cs
+++ Assets/Tutorials/09_RenderGraphBasics/Runtime/RenderGraphRuntime.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using GraphToolkitTutorials.RenderGraphBasics;
 
 namespace GraphToolkitTutorials.RenderGraphBasics.Runtime
 {
@@ -13,50 +12,13 @@ namespace GraphToolkitTutorials.RenderGraphBasics.Runtime
         /// <summary>
         /// 所有运行时节点
         /// </summary>
+        [SerializeReference]
         public List<RenderRuntimeNode> nodes = new List<RenderRuntimeNode>();
 
         /// <summary>
-        /// 相机节点索引
+        /// 起始节点索引
         /// </summary>
-        public int cameraNodeIndex = -1;
-
-        /// <summary>
-        /// 从编辑器图形构建运行时渲染图形
-        /// </summary>
-        public void BuildFromEditorGraph(RenderGraph editorGraph)
-        {
-            nodes.Clear();
-
-            // 转换所有节点
-            foreach (var editorNode in editorGraph.Nodes)
-            {
-                if (editorNode is RenderNode renderNode)
-                {
-                    var runtimeNode = renderNode.CreateRuntimeNode(editorGraph);
-                    nodes.Add(runtimeNode);
-
-                    // 记录相机节点索引
-                    if (editorNode is CameraNode)
-                    {
-                        cameraNodeIndex = nodes.Count - 1;
-                    }
-                }
-            }
-
-            Debug.Log($"Built runtime render graph with {nodes.Count} nodes, camera index: {cameraNodeIndex}");
-        }
-
-        /// <summary>
-        /// 获取指定索引的节点
-        /// </summary>
-        public T GetNode<T>(int index) where T : RenderRuntimeNode
-        {
-            if (index >= 0 && index < nodes.Count)
-            {
-                return nodes[index] as T;
-            }
-            return null;
-        }
+        public int startNodeIndex = -1;
 
         /// <summary>
         /// 获取指定索引的节点（非泛型版本）
@@ -64,18 +26,16 @@ namespace GraphToolkitTutorials.RenderGraphBasics.Runtime
         public RenderRuntimeNode GetNode(int index)
         {
             if (index >= 0 && index < nodes.Count)
-            {
                 return nodes[index];
-            }
             return null;
         }
 
         /// <summary>
-        /// 获取相机节点
+        /// 获取指定索引的节点（泛型版本）
         /// </summary>
-        public CameraNode GetCameraNode()
+        public T GetNode<T>(int index) where T : RenderRuntimeNode
         {
-            return GetNode<CameraNode>(cameraNodeIndex);
+            return GetNode(index) as T;
         }
     }
 }
